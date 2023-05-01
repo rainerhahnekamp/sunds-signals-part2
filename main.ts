a476169e962d4e7b8c5e6550bccb096568189eae
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-01 19:23:14 +0200

    add property binding feature

--- main.ts
+++ main.ts
@@ -6,12 +6,57 @@ abstract class AbstractComponent {
 
 type ComponentClass<Component extends AbstractComponent> = new () => Component;
 
+// ## Property Binding
+let currentBindingId = 0;
+
+function notNullable<T>(fn: () => T | undefined): NonNullable<T> {
+  const result = fn();
+  if (result === undefined || result === null) {
+    throw new Error("nullable");
+  }
+  return result;
+}
+
+function assertKeyOf<Component extends AbstractComponent>(
+  property: string | number | symbol,
+  component: Component
+): asserts property is keyof Component {
+  if (!(property in component)) {
+    throw new Error(`${String(property)} is not a property of ${component}`);
+  }
+}
+
+function setPropertyBindings<Component extends AbstractComponent>(
+  component: Component,
+  html: string
+) {
+  const bindingForId = new Map<
+    keyof Component,
+    { id: number; value: string }
+  >();
+  for (const [binding, name] of html.matchAll(/{{([a-z-]+)}}/g)) {
+    currentBindingId++;
+    assertKeyOf(name, component);
+    const value = String(component[name]);
+    bindingForId.set(name, {
+      id: currentBindingId,
+      value,
+    });
+    const placeholderTag = `<span id="ng-${currentBindingId}">${value}</span>`;
+    html = html.replace(binding, placeholderTag);
+  }
+  return { bindingPerId: bindingForId, html };
+}
+
 function bootstrapApplication<Component extends AbstractComponent>(
   appComponentClass: ComponentClass<Component>
 ) {
   window.addEventListener("load", () => {
     const appComponent = new appComponentClass();
-    document.body.innerHTML = appComponent.html;
+
+    const { bindingPerId: propertyBindingPerId, html: propertyBoundHtml } =
+      setPropertyBindings(appComponent, appComponent.html);
+    document.body.innerHTML = propertyBoundHtml;
   });
 }
 
@@ -20,10 +65,14 @@ class AppComponent extends AbstractComponent {
   constructor() {
     super(
       `<div>
-    <h1>Welcome</h1>
+    <h1>{{title}}</h1>
+    <p>{{time}}</p>
   </div>`
     );
   }
+
+  title = "Clock App";
+  time = new Date().toLocaleTimeString();
 }
 
 bootstrapApplication(AppComponent);
